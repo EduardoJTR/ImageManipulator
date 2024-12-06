49b82113ef332813d5437883f23210be23a5c261
EduardoJTR <edjo.terra@gmail.com>
2024-12-06 10:19:04 -0300

    Adicionado método flip

--- image.py
+++ image.py
@@ -21,3 +21,20 @@ class Image:
     def crop (self, start, end):
         self.store_secondary()
         self.main_data = self.main_data[start[0]:end[0], start[1]:end[1]]
+
+    # V for vertical and H for horizontal
+    def flip(self, direction):
+        self.secondary_data = self.main_data
+
+        if direction.lower() == 'v':
+            self.main_data = np.flip(self.main_data, 0)
+        elif direction.lower() == 'h':
+            self.main_data = np.flip(self.main_data, 1)
+
+    # Should receive the angle to rotate the image
+    def rotate(self, angle):
+        pass
+
+    # Should receive a tuple of the new dimensions
+    def resize(self, new_size):
+        pass
